1a95c5529f40cd1a4b6ff71d675db75334fb3260
zhangdayeye <zhangyejiu2024@outlook.com>
2025-07-21 23:09:58 +0800

    Update App.jsx, Home.jsx and add Home.css

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <React.Suspense fallback={<div>Loading...</div>}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
