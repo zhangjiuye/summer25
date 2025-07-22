fb75a979b7208c744425e39d122dc18f8a2c0048
zhangdayeye <zhangyejiu2024@outlook.com>
2025-07-22 18:41:14 +0800

    add login login out

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,88 @@
+import {
+  useEffect,
+  createContext,
+  useReducer,
+  useContext,
+  useState,
+} from "react";
 import "./App.css";
-import React from "react";
 import NavBar from "./components/NavBar";
+import About from "./screens/About";
+import Home from "./screens/Home";
+import Contact from "./screens/Contact";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PostDetail from "./screens/PostDetail";
 import AllPosts from "./screens/AllPosts";
-const Home = React.lazy(() => import("./screens/Home"));
-const About = React.lazy(() => import("./screens/About"));
-const Contact = React.lazy(() => import("./screens/Contact"));
-const CreatePost = React.lazy(() => import("./screens/CreatePost"));
-// const AllPosts = React.lazy(() => import("./screens/AllPosts"));
+import PostDetail from "./screens/PostDetail";
+import CreatePost from "./screens/CreatePost";
+import Signup from "./screens/Signup";
+import Login from "./screens/Login";
+import { initalUserState, userReducer } from "./reducers/userReducer";
+import MyAllPosts from "./screens/MyAllPosts";
+import React from "react";
+
+export const UserContext = createContext();
+
+function DynamicRoutes() {
+  const navigate = useNavigate();
+  const { state, dispatch } = useContext(UserContext);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      //user is already logged in
+      const user = JSON.parse(localStorage.getItem("user"));
+      const userState = { token: token, user: user };
+      const action = { type: "LOGIN", payload: userState };
+      dispatch(action);
+      navigate("/posts");
+    } else {
+      navigate("/login");
+    }
+  }, []);
+
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route exact path="/login" element={<Login />}></Route>
+      <Route exact path="/signup" element={<Signup />}></Route>
+      <Route exact path="/posts" element={<AllPosts />}></Route>
+      <Route exact path="/myposts" element={<MyAllPosts />}></Route>
+      <Route exact path="/create" element={<CreatePost />}></Route>
+      <Route
+        exact
+        path="/create/:postId/:userId"
+        element={<CreatePost />}
+      ></Route>
+      <Route
+        exact
+        path="/posts/:postId/:userId"
+        element={<PostDetail />}
+      ></Route>
+      <Route exact path="/about" element={<About />}></Route>
+      <Route exact path="/contact" element={<Contact />}></Route>
+    </Routes>
+  );
+}
 
 function App() {
-  return (
-    <Router>
-      <div className="app-container">
-        <NavBar />
-        <React.Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/posts" element={<AllPosts />} />
-            <Route path="/create" element={<CreatePost />} />
-            <Route path="/create/:postId/:userId" element={<CreatePost />} />
+  const [state, dispatch] = useReducer(userReducer, initalUserState);
 
-            <Route path="/posts/:postId/:userId" element={<PostDetail />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </React.Suspense>
-        <Footer />
-      </div>
-    </Router>
+  return (
+    <UserContext.Provider value={{ state: state, dispatch: dispatch }}>
+      <Router>
+        <div>
+          <NavBar></NavBar>
+          <DynamicRoutes />
+          <Footer />
+        </div>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
